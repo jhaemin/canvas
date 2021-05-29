adce45c9cfff37582d7b0886bc0c0b002bf46d9c
Jang Haemin <io@jhaemin.com>
2021-05-29 11:29:20 +0900

    Draw face with moveTo()

--- script.js
+++ script.js
@@ -18,9 +18,22 @@ function drawRedTriangle() {
   ctx.fill()
 }
 
-function draw() {
-  ctx.fillStyle = 'rgb(200, 0, 0)'
-  ctx.fillRect(10, 10, 50, 50)
+function drawSmileyFace() {
+  ctx.beginPath()
+  /**
+   * Outer circle
+   * x, y of arc() is the center of a circle
+   */
+  ctx.arc(75, 75, 50, 0, -Math.PI * 2, true)
+  ctx.moveTo(110, 75)
+  ctx.arc(75, 75, 35, 0, Math.PI, false) // Mouth (clockwise)
+  ctx.moveTo(65, 65)
+  ctx.arc(60, 65, 5, 0, Math.PI * 2, true)
+  ctx.moveTo(95, 65)
+  ctx.arc(90, 65, 5, 0, Math.PI * 2, true)
+  ctx.stroke()
+}
+
 
   ctx.fillStyle = 'rgba(0, 0, 200, 0.3)'
   ctx.fillRect(30, 30, 50, 50)
