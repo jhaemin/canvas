5b5ad4c8da429568f0ab141259b67bb32c86c132
Jang Haemin <io@jhaemin.com>
2021-05-29 11:29:04 +0900

    Draw triangle

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const canvas = document.getElementById('canvas')
 
 const ctx = canvas.getContext('2d')
 
-console.log(ctx)
+function example1() {
+  ctx.fillRect(25, 25, 100, 100)
+  ctx.clearRect(45, 45, 60, 60)
+  ctx.strokeRect(50, 50, 50, 50)
+}
+
+function drawRedTriangle() {
+  ctx.beginPath()
+  ctx.fillStyle = '#ff0000'
+  ctx.moveTo(75, 50)
+  ctx.lineTo(100, 75)
+  ctx.lineTo(100, 25)
+  ctx.fill()
+}
 
 function draw() {
   ctx.fillStyle = 'rgb(200, 0, 0)'
