2671bd545a03d675ae2e6947eefd44539299c0dc
Jang Haemin <io@jhaemin.com>
2021-05-30 10:35:09 +0900

    Draw arcs

--- script.js
+++ script.js
@@ -49,4 +49,26 @@ function drawLines() {
   ctx.stroke()
 }
 
-drawLines()
+function drawArcs() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 3; j++) {
+      ctx.beginPath()
+      const x = 25 + j * 50 // x coordinate
+      const y = 25 + i * 50 // y coordinate
+      const radius = 20 // Arc radius
+      const startAngle = 0 // Starting point on circle
+      const endAngle = Math.PI + (Math.PI * j) / 2 // End point on circle
+      const counterclockwise = i % 2 !== 0 // clockwise or counterclockwise
+
+      ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise)
+
+      if (i > 1) {
+        ctx.fill()
+      } else {
+        ctx.stroke()
+      }
+    }
+  }
+}
+
+drawArcs()
