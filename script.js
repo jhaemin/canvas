b531a615cf442f9e20b2948931af9c4437d5ee9c
Jang Haemin <io@jhaemin.com>
2021-06-01 20:07:15 +0900

    Path2D

--- script.js
+++ script.js
@@ -186,4 +186,15 @@ function drawPacMan() {
   ctx.fill()
 }
 
-drawPacMan()
+function path2DExample() {
+  const rectangle = new Path2D()
+  rectangle.rect(10, 10, 50, 50)
+
+  const circle = new Path2D()
+  circle.arc(100, 35, 25, 0, 2 * Math.PI)
+
+  ctx.stroke(rectangle)
+  ctx.fill(circle)
+}
+
+path2DExample()
